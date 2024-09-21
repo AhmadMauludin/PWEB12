d8b92e72fb7a89190c668d58a9dd9a197c76e999
AhmadMauludin <ahmadmaldin1799@gmail.com>
2024-09-21 15:37:33 +0700

    pad

--- menu/menu.php
+++ menu/menu.php
@@ -10,3 +10,9 @@
               <i class="glyphicon glyphicon-user"></i>
               Data User </a>
       </div>
+
+      <div class="navbar-header">
+          <a class="navbar-brand" href="logout.php">
+              <i class="glyphicon glyphicon-off"></i>
+              Logout </a>
+      </div>
