349c2b0a0f5e06f33b3dc385fbb2f310a43868ac
ahmadmaldin <ahmad.mauludin247@guru.smk.belajar.id>
2024-09-29 17:02:52 +0700

    poipoi

--- config/dispekael.sql
+++ config/dispekael.sql
@@ -2,7 +2,7 @@
 -- Database Backup --
 -- Ver. : 1.0.1
 -- Host : 127.0.0.1
--- Generating Time : Sep 27, 2024 at 23:31:35:PM
+-- Generating Time : Sep 28, 2024 at 10:19:34:AM
 
 
 CREATE TABLE `pegawai` (
@@ -43,11 +43,24 @@ CREATE TABLE `seminar` (
   `nilailaporan` int(3) NOT NULL,
   `nilaiaplikasi` int(3) NOT NULL,
   PRIMARY KEY (`idseminar`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 INSERT INTO seminar VALUES
-("5","120220795","Aplikasi Berbasis Web","1578981951_File Seminar.pdf","github.com/AhmadMauludin/PWEB12","Disetujui","Selamat seminar","3","4","78","0","85"),
-("6","120220803","Aplikasi Berbasis Web","1053844150_pdf-proposal-kunjungan-industri-2023_compress.pdf","github.com/AhmadMauludin/PKK11","Disetujui","Selamat seminar","3","4","0","95","0");
+("5","120220795","Aplikasi Berbasis Web","1578981951_File Seminar.pdf","github.com/AhmadMauludin/PWEB12","Disetujui","Selamat seminar","3","4","78","50","85"),
+("7","120220823","Aplikasi Berbasis Web","1885701658_File Seminar.pdf","github.com/AhmadMauludin/PWEB12","Disetujui","Selamat seminar icaa","3","4","70","65","80"),
+("8","120220826","Aplikasi Seminar Prakerin Berbasis Web","549492775_File Seminar.pdf","github.com/AhmadMauludin/PWEB12","Disetujui","Selamat seminar","3","4","90","93","78"),
+("9","220307808","C_SHIRT","2074182362_pdf-proposal-kunjungan-industri-2023_compress.pdf","Github/AhmadMauludin/PWEB12","Disetujui","Selamat seminar","3","4","0","0","0"),
+("10","11112222","surat keluar masuk","379017897_JADWAL SEM GANJIL 20242025 Episode 3.pdf","github.com/AhmadMauludin/PWEB12","Disetujui","selamat seminar silva","3","4","0","0","0"),
+("11","12131415","aplikasi absensi pegawai ","324984689_KEBUTUHAN BASIS DATA_240923_142615.pdf","github/Ahmad Mauludin/PWEB12","Disetujui","selamat seminar zakiyah","3","4","0","0","0"),
+("12","20807","Pengesahan sk pokdarwis ","1231838172_Selamat Datang.pdf","Github/Ahmad Mauludin/PWEB12","Disetujui","selamat seminar ","3","4","0","0","0"),
+("13","120220785","Sistem Manajemen Pelayanan Informasi Pegawai","364843864_File Seminar.pdf","github.com/ghaisanailal/programprisma","Disetujui","selamat seminar ","3","4","90","95","100"),
+("14","708","Asinpedas","2050496011_Cokelat Minimalis Surat izin muhadlhoroh_20240906_094442_0000.pdf","Github/AhmadMauludin/PWEB12","Disetujui","Selamat seminar otu","3","4","0","70","0"),
+("15","120220817","Aplikasi berbasis web ","312836948_Surat_Perintah_1724211197818_Lomba_Agustus_2024.pdf","Github/Ahmad Mauludin/PWEB12","Disetujui","Sok","3","4","0","0","0"),
+("16","120220792","Aplikasi Berbasis Web","1533459044_B-2024.pdf","Github/ahmadmauludin/PWEB12","Disetujui","Gaskeun","3","4","0","0","0"),
+("17","120220829","aplikasi berbasis web","1247484288_Selamat Datang.pdf","github.com/AhmadMauludin/PWEB12","Disetujui","Manga","3","4","0","0","0"),
+("18","120220831","Aplikasi Berbasis Web","1581546092_Ahmad_Ismail_Semua_Bisa_Menulis_Kaligrafi.pdf","Github/ahmadmauludin/PWEB12","Disetujui","Ok","3","4","0","0","0"),
+("19","120220822","aplikasi inventaris dan sistem monitoring suhu dan kelembaban alat standar di uptd metrologi kab. Sumedang","1913830915_File Seminar.pdf","github.com/AhmadMauludin/PWEB12","Disetujui","Ok","3","4","0","0","0"),
+("20","98765","Aplikasi Santri","2017207302_File Seminar.pdf","github.com/AhmadMauludin/PWEB12","Disetujui","U","3","4","99","0","0");
 
 CREATE TABLE `user` (
   `id` bigint(12) NOT NULL AUTO_INCREMENT,
@@ -56,7 +69,7 @@ CREATE TABLE `user` (
   `level` varchar(30) NOT NULL,
   `nis` varchar(9) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=120220804 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1284567953666 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 INSERT INTO user VALUES
 ("1","ahmadmauludin","ahmad","Admin","1"),
@@ -70,8 +83,25 @@ INSERT INTO user VALUES
 ("9","ninamarlina","nina","Pegawai","9"),
 ("10","donimulyadi","doni","Pegawai","10"),
 ("11","ernasrimeilani","erna","Pegawai","11"),
+("708","Syifaqurrotuaini","azaamine","User","0708"),
+("20807","Fauzy","ahmad","User","020807"),
+("98765","Maulana","maulana","User","98765"),
+("11112222","silva melani","anakbaik","User","11112222"),
+("12131415","zasri","september123","User","12131415"),
+("120220785","ghaitsa","ghaitsa","User","120220785"),
+("120220792","Alfi Siti Fauziyyah","mpicantik","User","120220792"),
 ("120220795","muhgra","agra","User","120220795"),
-("120220803","sarahamelia","sarah","User","120220803");
+("120220817","Muhammad Najib Husaini Fathurr","husen","User","120220817"),
+("120220822","Nidia Gitania","nidia","User","120220822"),
+("120220823","nisatia","nisanovember","User","120220823"),
+("120220826","siti nurazizah","sitinurazizah","User","120220826"),
+("120220829","satiaaa","zahraa","User","120220829"),
+("120220831","Winda eka sawitri","oktober","User","120220831"),
+("120220833","Intan Nuraeni","intan","User","120220833"),
+("123456789","zaasriicii","zakiyyahpk123","User","123456789"),
+("2203078080","Sarahh","123sukses","User","220307808"),
+("121314151617","suci silva","anakbaik123","User","121314151"),
+("1284567953665","zasrii","zakiyyah12345","User","128456795");
 
 CREATE TABLE `users` (
   `nis` bigint(12) unsigned NOT NULL,
@@ -87,5 +117,19 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 INSERT INTO users VALUES
+("708","708","Syifa Qurrotu Aini","2007-03-13","Dsn. Campaka, Ds. Sundamekar, RT 02, RW 04, Kec. Cisitu, Kab. Sumedang","Diskominfosanditik","625388776699","Aktif","460311946_IMG-20240928-WA0009.jpg"),
+("20807","20807","Fauzy ahmad","2007-08-02","Darmaking","Disparbudpora","082295153586","Aktif","1745402583_IMG-20240927-WA0002.jpg"),
+("98765","98765","Maulana Uciha","2024-09-28","Konohagakure","BPKAD","56756757575","Aktif","378968302_1000_F_435340582_8yoV1uzvHNOYzddAgHZuXJgSdquFrkJp.jpg"),
+("11112222","11112222","silva melani","2024-05-01","tanjungkerta","BPKAD","081320186397","Aktif","462476662_2f851c1ba7be1f243a3ddbaa88a8bf27.jpg"),
+("12131415","12131415","zakiyyah","2024-09-27","subang","Disdukcapil","081224719095","Aktif","1794461544_c2ef133c8396ae1cd6315fac14d0de8a.jpg"),
+("120220785","120220785","Ghaisa Nailal Faroha","2007-11-02","Tanjungsari","Kementerian Agama","0857111111","Aktif","1667805050_1696307224982-removebg-preview.png"),
+("120220792","120220792","Alfi Siti Fauziyyah","2006-11-02","Rancakalong","Diskominfosanditik","6282216448153","Aktif","517453720_twice_logo_by_mimilevi_ddz8buj-fullview.png"),
 ("120220795","120220795","Muhamad Agra Rizkia Mulyana","2006-01-01","Sumedang Jawa Barat","Diskominfosanditik","628999000777","Aktif","646086561_city.png"),
-("120220803","120220803","Sarah Amelia Anugrah","2006-06-30","Soreang - Bandung","Diskominfosanditik","628567788998","Aktif","163874413_WhatsApp Image 2023-08-24 at 21.54.38.jpeg");
+("120220817","120220817","Muhammad Najib Husaini Fathurrohman ","2007-09-14","Dusun.sukaluyu,desa.cilengkrang,kecamatan.wado, kabupaten.sumedang","Dinas Pendidikan","081397746333","Aktif","809862115_IMG-20240927-WA0002.jpg"),
+("120220822","120220822","Nidia Gitania","2006-11-14","Sukasari","Diskoperindag","085123456789","Aktif","613257983_pratama-arhan-320x_.png"),
+("120220823","120220823","Nisa Nuraeni","2007-11-15","Lingkungan Hegarmanah","Disparbudpora","6285794956470","Aktif","189-1696307224982.jpg"),
+("120220826","120220826","Siti nurazizah","2006-10-20","Lingkungan Tanjungsari","Dinas Pendidikan","6282315917436","Aktif","2140118128_1696307224982.jpg"),
+("120220829","120220829","Syatia Az - Zahra Virgiani","2007-03-20","Jatigede","Disparbudpora","081222057496","Aktif","2043409910_444e0df3ba653549aa3ae22ff168708c_1727246574041_0.webp.jpg"),
+("120220831","120220831","Winda eka sawitri","2006-10-29","Sembir","Dinas Pendidikan","6281901353858","Aktif","1617258577_twice_logo_by_mimilevi_ddz8buj-fullview.png"),
+("120220833","120220833","Intan Nuraeni","2006-05-27","Rancakalong","Diskoperindag","081319772756","Aktif","1706418275_pratama-arhan-320x_.png"),
+("220307808","2203078080","Sarah Amelia Anugrah","2007-03-22","Kertasari","Diskominfosanditik","0832384149","Aktif","486950069_242659596_109930898111134_7838980731149597570_n_121404.jpg");
